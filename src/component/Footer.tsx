3c5458b3b3389fd05cc93398174defc5f3b3fb54
184744 <184744@ssg.com>
2021-07-29 14:55:34 +0900

    푸터변경

--- src/component/Footer.tsx
+++ src/component/Footer.tsx
@@ -3,12 +3,13 @@ import React from "react";
 
 const Footer = () => {
 	return (
-		<Box p={4} align="center" style={{ display: "none" }} display={{ md: "block !important" }}>
+		<Box p={4} align="center" style={{ display: "none" }} display={{ md: "block !important" }} fontSize="sm">
 			제공: 공공데이터포털 (
 			<a href="https://www.data.go.kr/index.do" target="_blank" rel="noreferrer">
 				https://www.data.go.kr/index.do
 			</a>
-			)<p>alexsando</p>
+			)<p>2021-07-19 ~ 2021-07-29</p>
+			<p>CREATOR: ALEXSANDO</p>
 		</Box>
 	);
 };
